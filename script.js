8ce4f2a9308eea89a7e4bb52e200ca4bb3be788b
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-12-04 13:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+let imageUrls=[images[0].url,images[1].url,images[2].url];
+
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        let img = document.createElement('img');
+        img.src = URL.createObjectURL(blob);
+        resolve(img);
+      })
+      .catch(error => reject(`Failed to load image's URL: ${url}`));
+  });
+}
+
+document.getElementById('download-images-button').addEventListener('click', () => {
+  Promise.all(imageUrls.map(url => loadImage(url)))
+    .then(images => {
+      let output = document.getElementById('output');
+      images.forEach(img => output.appendChild(img));
+    })
+    .catch(error => console.error(error));
+});
